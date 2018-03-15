0b477be421b91bb0a5c988fdafe5a98b01d471e7
Patrick East <patrick.east@icloud.com>
2018-03-15 13:30:18 -0600

    Buttons added and functionality working.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,14 +12,34 @@ var topics = [
     "The Academy Is...",
     "Motion City Soundtrack"
 ];
-$("searchBtn").on("click", function () {
+
+function makeButtons() {
+
+    $("#giphyContainer").empty();
+
     for (var i = 0; i < topics.length; i++) {
         var newButton = $("<button>");
-        newButton.attr("class", "newBtn");
-        topics[i].append(newButton);
-    };
+        newButton
+            .addClass("newBtn")
+            .attr("data-name", topics[i])
+            .text(topics[i]);
+        $("#giphyContainer").append(newButton);
+    }
+}
+
+$("#searchBtn").on("click", function (event) {
+
+    event.preventDefault();
+
+    var topic = $("#topic-input").val().trim();
+
+    topics.push(topic);
+
+    makeButtons();
+
 });
 
+makeButtons();
 // $("searchBtn").on("click", function () {
 //     var topics = $(".form-control").attr("data", "object");
 //     var newButton = $("<button>");
