2b1a1f3539f8fa60af6fc90d5b8f3577d46c569b
Patrick East <patrick.east@icloud.com>
2018-03-14 21:05:20 -0600

    Added topics array and for-loop.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,28 @@
 // API KEY = kP5Rfl1iakIqhhB6Zqtlndtr4azKZ3s2
 
-var topics = [];
+var topics = [
+    "Yellowcard",
+    "New Found Glory",
+    "The Ataris",
+    "Boys Like Girls",
+    "My Chemical Romance",
+    "American Hi-Fi",
+    "Taking Back Sunday",
+    "Cute is What We Aim For",
+    "The Academy Is...",
+    "Motion City Soundtrack"
+];
+$("searchBtn").on("click", function () {
+    for (var i = 0; i < topics.length; i++) {
+        var newButton = $("<button>");
+        newButton.attr("class", "newBtn");
+        topics[i].append(newButton);
+    };
+});
+
+// $("searchBtn").on("click", function () {
+//     var topics = $(".form-control").attr("data", "object");
+//     var newButton = $("<button>");
+//     newButton.attr("class", "newBtn");
+//     topics[i].append(newButton);
+// })
